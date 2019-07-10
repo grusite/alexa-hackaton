ed8344af53a157d1f0fc3889857148747c13e40a
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-10 10:54:30 +0200

    check slots

--- handlers/parrillaHandler.js
+++ handlers/parrillaHandler.js
@@ -6,6 +6,16 @@ module.exports = tarifasVodafoneHandler = {
 	handle(handlerInput) {
 		const speechText = 'Estás en la parrilla';
 
+		const slots = handlerInput.requestEnvelope.request.slots;
+
+		// "dime que {tipo} de {subgenre} {orden} {tiempo} a las {horario}"
+
+		console.log(slots['tipo']);
+		console.log(slots['subgenre']);
+		console.log(slots['tiempo']);
+		console.log(slots['horario']);
+		
+
 		return handlerInput.responseBuilder
 				.speak(speechText)
 				.withSimpleCard('Hello World', speechText)
