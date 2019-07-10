592ff1bff2124a8bac7ad9a932ecfd4dd235cd84
ALONSO, JOSEBA IKER, Vodafone Spain <josebaiker.alonso@vodafone.com>
2019-07-10 15:37:34 +0200

    ahora modelo

--- handlers/terminales/vodafoneTerminalesInitHandler.js
+++ handlers/terminales/vodafoneTerminalesInitHandler.js
@@ -33,4 +33,23 @@ const vodafoneMarcaIntentHandler = {
   }
 };
 
-module.exports = [vodafoneTerminalesInitHandler,vodafoneMarcaIntentHandler]
+const vodafoneMarcaContentHandler = {
+  canHandle(handlerInput) {
+    return (
+            handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+            handlerInput.requestEnvelope.request.intent.name === "vodafoneTerminalesInit" &&
+            handlerInput.requestEnvelope.request.intent.slots.precioMarca.value &&
+            handlerInput.requestEnvelope.request.intent.slots.precioMarca.value === "marca" &&
+            handlerInput.requestEnvelope.request.intent.slots.marca.value &&
+            handlerInput.requestEnvelope.request.intent.slots.modelo.value == null
+    );
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+            .speak("¿Qué modelo quieres?")
+            .reprompt("¿Qué modelo quieres?")
+            .addElicitSlotDirective("modelo")
+            .getResponse();
+  }
+};
+module.exports = [vodafoneTerminalesInitHandler,vodafoneMarcaIntentHandler,vodafoneMarcaContentHandler]
