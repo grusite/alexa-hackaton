2b9cb3d8e2bdc6b3162b507cc953c0ab39d27686
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-10 10:33:21 +0200

    parrilla to guapa

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const LaunchRequestHandler = require("./handlers/LaunchRequestHandler");
 // Nuestros Handler
 const VodadoneTestOneHandler = require("./handlers/VodafoneTestOnerHandler");
 const DefaultOneHandler = require("./handlers/DefaultOneHandler");
+const ParrillaHandler = require("./handlers/parrillaHandler");
 
 // Built-in core
 const HelpIntentHandler = require("./handlers/HelpIntentHandler");
@@ -22,6 +23,7 @@ exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
     VodadoneTestOneHandler,
+    ParrillaHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
