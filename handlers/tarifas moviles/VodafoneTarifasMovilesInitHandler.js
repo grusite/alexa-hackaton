46cd0adb19c304ca7528afc0da83e870871a9029
pnyxis <jlasso@divisadero.es>
2019-07-10 17:45:57 +0200

    Update VodafoneTarifasMovilesInitHandler.js

--- handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
+++ handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
@@ -7,7 +7,7 @@ const = VodafoneTarifasMovilesInitHandler = {
       			handlerInput.requestEnvelope.request.dialogState !== "COMPLETED");
 	},
 	handle(handlerInput) {
-		const speechText = 'Genial. Vamos a ver qué tarifa móvil se adapta mejor a ti. ¿Sueles jugar cada día online?';
+		//const speechText = 'Genial. Vamos a ver qué tarifa móvil se adapta mejor a ti. ¿Sueles jugar cada día online?';
 
 		return handlerInput.responseBuilder
       			.addDelegateDirective()
