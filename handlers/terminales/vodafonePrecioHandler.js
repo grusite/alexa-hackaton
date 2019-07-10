09e664042862cbe5d4e86e1b8667e6ee27732678
PARRES, JORGE LUIS, Vodafone Spain <jorgeluis.parres@vodafone.com>
2019-07-10 16:45:45 +0200

    Fix parseFloat

--- handlers/terminales/vodafonePrecioHandler.js
+++ handlers/terminales/vodafonePrecioHandler.js
@@ -30,7 +30,7 @@ const vodafonePrecioHandler = {
     },
     handle(handlerInput) {
       let precio = handlerInput.requestEnvelope.request.intent.slots.precio.value;
-      const query = terminales.getTerminalsByPrice(precio);
+      const query = terminales.getTerminalsByPrice(parseFloat(precio));
       const terminal = query[0];
       const text = `He encontrado el terminal ${terminal.marca} ${terminal.modelo} a un precio de ${terminal.cuotaMensualConIva} euros, 
       con la tarifa ${query.nombreTarifa}, te interesa?`;
