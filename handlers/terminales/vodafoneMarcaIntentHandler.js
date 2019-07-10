9a27c81a82f24eed55c8df563b05db1564cd304c
PARRES, JORGE LUIS, Vodafone Spain <jorgeluis.parres@vodafone.com>
2019-07-10 13:16:31 +0200

    Filtro no valor marca / precio

--- handlers/terminales/vodafoneMarcaIntentHandler.js
+++ handlers/terminales/vodafoneMarcaIntentHandler.js
@@ -2,11 +2,10 @@ module.exports = vodafoneMarcaIntentHandler = {
   canHandle(handlerInput) {
     return (
       handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name ===
-        "vodafoneTerminalesInit" &&
+      handlerInput.requestEnvelope.request.intent.name === "vodafoneTerminalesInit" &&
       handlerInput.requestEnvelope.request.intent.slots.precioMarca.value &&
-      handlerInput.requestEnvelope.request.intent.slots.precioMarca.value ===
-        "marca"
+      handlerInput.requestEnvelope.request.intent.slots.precioMarca.value === "marca" &&
+      !handlerInput.requestEnvelope.request.intent.slots.marca.value
     );
   },
   handle(handlerInput) {
