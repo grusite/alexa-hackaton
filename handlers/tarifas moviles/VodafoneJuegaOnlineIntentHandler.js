61292716e2b5bf329364e48518c8dd62484c6fa0
pnyxis <jlasso@divisadero.es>
2019-07-10 17:13:21 +0200

    Update VodafoneJuegaOnlineIntentHandler.js

--- handlers/tarifas moviles/VodafoneJuegaOnlineIntentHandler.js
+++ handlers/tarifas moviles/VodafoneJuegaOnlineIntentHandler.js
@@ -1,6 +1,5 @@
 module.export = VodafoneJuegaOnlineIntentHandler = {
   canHandle(handlerInput) {
-    console.log("Entro en JuegasOnline")
     return (
       handlerInput.requestEnvelope.request.type === "IntentRequest" &&
       handlerInput.requestEnvelope.request.intent.name ===
@@ -12,12 +11,14 @@ module.export = VodafoneJuegaOnlineIntentHandler = {
   },
   handle(handlerInput) {
 
+    console.log("Entro en JuegasOnline")
+
     const text = "Como buen Gamer, creo que la tarifa que más se adapta es la Ilimitada Movil Total porque lleva la fibra con un 1Gbps simétrico para todos tus dispositivos y datos ilimitados 5G para jugar con la Switch donde queráis"
 
     return handlerInput.responseBuilder
       .speak(text)
       .reprompt(text)
-      .addElicitSlotDirective("juegasOnline")
+      //.addElicitSlotDirective("juegasOnline")
       .getResponse();
   }
 };
