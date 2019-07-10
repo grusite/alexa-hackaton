c0c940fba4fd4b1a077ec9b45217b9cf8bd2b694
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-10 19:37:40 +0200

    tv

--- handlers/tv/parrillaTipoContentHandler.js
+++ handlers/tv/parrillaTipoContentHandler.js
@@ -10,11 +10,9 @@ module.exports = parrillaTipoContentHandler = {
 		const slots = handlerInput.requestEnvelope.request.intent.slots;
 		let searchResult, speechText;
 
-		speechText = 'Ka pachao?';
-
-		/*searchResult = search(slots);
+		searchResult = search(slots);
 		speechText = searchResult.length > 0 ? 'Tenemos el siguiente resultado: ' : 'No hemos encontrado nada';
-		searchResult.forEach(pos => {
+		/*searchResult.forEach(pos => {
 			speechText += `${pos.title} en ${pos.canal}, `;
 		});*/
 
