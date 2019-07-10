ca8e1c0efc512059f9536228aafeb0a66345c0e9
Eugen Marius Circiu <admin.pay4surf@gmail.com>
2019-07-10 22:45:29 +0200

    Update VodafoneTarifasMovilesInitHandler.js

--- handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
+++ handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
@@ -15,6 +15,34 @@ const VodafoneTarifasMovilesInitHandler = {
 	}
 };
 
+const VodafoneJuegaOnlineIntentHandler = {
+
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name ===
+        "vodafoneTarifasMovilesInit" &&
+      handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value &&
+      handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value ===
+        "si" &&
+      handlerInput.requestEnvelope.request.intent.slots.contratarTarifa.value == null &&
+      handlerInput.requestEnvelope.request.intent.slots.usoDatos.value == null &&
+      handlerInput.requestEnvelope.request.intent.slots.isLlamadasMasSeisMinutos.value == null
+    );
+  },
+  handle(handlerInput) {
+
+    //console.log("Entro en JuegasOnline")
+
+    const text = "Como buen Gamer, creo que la tarifa que más se adapta es la Ilimitada Movil Total porque lleva la fibra con un 1Gbps simétrico para todos tus dispositivos y datos ilimitados 5G para jugar con la Switch donde queráis";
+
+    return handlerInput.responseBuilder
+      .speak(text)
+      .reprompt(text)
+      .addElicitSlotDirective("juegasOnline")
+      .getResponse();
+  }
+};
 const VodafoneTarifasMovilesOrderIntentHandler = {
   canHandle(handlerInput) {
     console.log("aquí está tu order")
@@ -45,35 +73,6 @@ const VodafoneTarifasMovilesOrderIntentHandler = {
         .getResponse();
   }
 };
-
-const VodafoneJuegaOnlineIntentHandler = {
-
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name ===
-        "vodafoneTarifasMovilesInit" &&
-      handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value &&
-      handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value ===
-        "si" &&
-      handlerInput.requestEnvelope.request.intent.slots.usoDatos.value == null &&
-      handlerInput.requestEnvelope.request.intent.slots.isLlamadasMasSeisMinutos.value == null
-    );
-  },
-  handle(handlerInput) {
-
-    //console.log("Entro en JuegasOnline")
-
-    const text = "Como buen Gamer, creo que la tarifa que más se adapta es la Ilimitada Movil Total porque lleva la fibra con un 1Gbps simétrico para todos tus dispositivos y datos ilimitados 5G para jugar con la Switch donde queráis";
-
-    return handlerInput.responseBuilder
-      .speak(text)
-      .reprompt(text)
-      .addElicitSlotDirective("juegasOnline")
-      .getResponse();
-  }
-};
-
 const VodafoneNoJuegaOnlineIntentHandler = {
   canHandle(handlerInput) {
     return (
