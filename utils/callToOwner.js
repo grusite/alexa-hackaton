dd44f57157474120fca752ce8ee372b036bfdc25
Eugen Marius Circiu <admin.pay4surf@gmail.com>
2019-07-10 23:00:26 +0200

    Contratación con agente pesao :D

--- utils/callToOwner.js
+++ utils/callToOwner.js
@@ -1,11 +1,20 @@
-async function callToOwner(){
+const PERMISSIONS = [
+	'alexa::profile:given_name:read', // Permisos para pedirle acceso al nombre
+	'alexa::profile:email:read', // Permisos para pedirle acceso al email
+	'alexa::profile:mobile_number:read', // Permisos para pedirle acceso al numero de móvil
+	'alexa::alerts:reminders:skill:readwrite' // permisos para los reminders
+];
+const axios = require("axios");
+
+async function callToOwner(handlerInput){
+	const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
 	const { deviceId } = requestEnvelope.context.System.device;
 	const UpsServiceClient = serviceClientFactory.getUpsServiceClient();
-	const name = await UpsServiceClient.getProfileGivenName();
-	const email = await UpsServiceClient.getProfileEmail();
+	/*const name = await UpsServiceClient.getProfileGivenName();
+	const email = await UpsServiceClient.getProfileEmail();*/
 	const mobile = await UpsServiceClient.getProfileMobileNumber();
 
-	let nro = '610513459';
+	let nro = '610514392';
 	console.log("Iniciamos llamada al "+nro);
 	//console.log("Iniciamos llamada al "+mobile.phoneNumber);
 	const url = 'https://ws.walmeric.com/provision/wsclient/client_addlead.html?c_var_2=vodafone-one&phone='+nro+'&verifyLeadId=NO&boton-flotante=Si&url=&check=on&url=https%3A%2F%2Fwww.vodafone.es%2Fc%2Ftienda-online%2Fparticulares%2Ftarifas-moviles%2Ftarifa-movil-1%2F&URL_sin_parametro=https%3A%2F%2Fwww.vodafone.es%2Fc%2Ftienda-online%2Fparticulares%2Ftarifas-moviles%2Ftarifa-movil-1%2F&idTag=29842f94d414949bf95fb2e6109142cfef1fb2a78114c2c536a36bf5a65b953a5fc1ca581ceb7bf8fd143a36f4eb693794c0f95c76664a12dbaa532a82b5988a207dd2d28aa08a82723b60cd54ef0e48b3a24e4b4ad1c4968e7803e10bb5ed21b0441305a1103889a0fa0536abc02d8c&format=json';
