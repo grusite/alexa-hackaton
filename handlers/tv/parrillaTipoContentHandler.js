340cb222134d911d05878b9c17522d14d12f13d8
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-10 22:01:31 +0200

    marius <3

--- handlers/tv/parrillaTipoContentHandler.js
+++ handlers/tv/parrillaTipoContentHandler.js
@@ -8,13 +8,32 @@ module.exports = parrillaTipoContentHandler = {
 	},
 	handle(handlerInput) {
 		const slots = handlerInput.requestEnvelope.request.intent.slots;
-		let searchResult, speechText;
+		let searchResult, searchLength, speechText;
 
 		searchResult = search(slots);
-		speechText = searchResult && searchResult.length > 0 ? 'Tenemos el siguiente resultado: ' : 'No hemos encontrado nada';
-		searchResult.forEach(pos => {
+		searchLength = searchLength.length
+
+		if(searchResult && searchLength > 0) {
+			let random = 0;
+			speechText = 'Tenemos el siguiente resultado: ';
+
+			for(let i = 3; i > 0 && i < searchLength - 1; i--) {
+				random = parseInt(Math.random() + searchLength);
+				speechText += `${searchResult[random].title} en ${pos.canal}, `;
+				searchResult.splice(random, 1);
+			}
+
+			speechText += searchResult.length > 3 ? `quiere ver más ${slots.tipo.value}?` : '';
+			handlerInput.attributesManager.setSessionAttributes({searchResult});
+
+		} else {
+			speechText = 'No hemos encontrado nada';
+		}
+		
+
+		/*searchResult.forEach(pos => {
 			speechText += `${pos.title} en ${pos.canal}, `;
-		});
+		});*/
 
 		return handlerInput.responseBuilder
 			.speak(speechText)
