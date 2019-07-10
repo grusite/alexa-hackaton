fb41b59b484bba9e0e1a6e3b8da64365bad56156
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-10 18:38:55 +0200

    merge

---
+++ handlers/tv/parrillaTipoContentHandler.js
@@ -0,0 +1,25 @@
+const search = require('../../back/tvBack');
+
+module.exports = parrillaTipoContentHandler = {
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+				&& handlerInput.requestEnvelope.request.intent.name === 'vodafoneTv'
+				&& handlerInput.requestEnvelope.request.intent.slot.tipo.value;
+	},
+	handle(handlerInput) {
+		const slots = handlerInput.requestEnvelope.request.intent.slots;
+		let searchResult, speechText;
+
+		searchResult = search(slots);
+		speechText = searchResult.length > 0 ? 'Tenemos el siguiente resultado: ' : 'No hemos encontrado nada';
+		searchResult.forEach(pos => {
+			speechText += `${pos.title} en ${channelData[pos.canal].name}, `;
+		});
+
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+        	.addElicitSlotDirective("tipo")
+			.getResponse();
+	},
+};
