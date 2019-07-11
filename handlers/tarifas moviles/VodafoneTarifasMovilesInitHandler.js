1a388f3d8875601c2ea694510477fb62cf69065a
Eugen Marius Circiu <admin.pay4surf@gmail.com>
2019-07-11 10:50:02 +0200

    Update VodafoneTarifasMovilesInitHandler.js

--- handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
+++ handlers/tarifas moviles/VodafoneTarifasMovilesInitHandler.js
@@ -23,7 +23,9 @@ const VodafoneJuegaOnlineIntentHandler = {
       handlerInput.requestEnvelope.request.type === "IntentRequest" &&
       handlerInput.requestEnvelope.request.intent.name === "vodafoneTarifasMovilesInit" &&
       handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value &&
-      handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value === "si" &&
+      (handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value.id === "si" ||
+          handlerInput.requestEnvelope.request.intent.slots.juegasOnline.value.name === "si"
+      ) &&
       handlerInput.requestEnvelope.request.intent.slots.contratarTarifa.value == null &&
       handlerInput.requestEnvelope.request.intent.slots.usoDatos.value == null &&
       handlerInput.requestEnvelope.request.intent.slots.isLlamadasMasSeisMinutos.value == null
