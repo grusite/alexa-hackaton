479c22bba53c161fc5ef7c67eeb949b3ec9b7797
victorquality <36501200+victorquality@users.noreply.github.com>
2019-07-11 09:25:08 +0200

    tv

--- handlers/tv/parrillaTipoContentHandler.js
+++ handlers/tv/parrillaTipoContentHandler.js
@@ -5,7 +5,7 @@ module.exports = parrillaTipoContentHandler = {
 		return handlerInput.requestEnvelope.request.type === 'IntentRequest'
 				&& handlerInput.requestEnvelope.request.intent.name === 'vodafoneTv'
 				&& handlerInput.requestEnvelope.request.intent.slots.tipo.value
-				&& handlerInput.requestEnvelope.request.intent.slots.mas.value === null;
+				&& !handlerInput.requestEnvelope.request.intent.slots.mas.value;
 	},
 	handle(handlerInput) {
 		const slots = handlerInput.requestEnvelope.request.intent.slots;
