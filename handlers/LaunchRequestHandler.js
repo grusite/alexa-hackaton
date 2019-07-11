441f850113188a1787aaa5539fe5602a63e11223
Eugen Marius Circiu <admin.pay4surf@gmail.com>
2019-07-11 10:08:30 +0200

    Update LaunchRequestHandler.js

--- handlers/LaunchRequestHandler.js
+++ handlers/LaunchRequestHandler.js
@@ -5,7 +5,12 @@ module.exports = LaunchRequestHandler = {
 	},
 	async handle(handlerInput) {
 		//let accessToken = this.event.context.System.apiAccessToken;
-		const speechText = 'Bienvenido a Vodafone Skill';
+		const speechText = "<speak>
+			<prosody volume=""x-loud"">Bienvenido,</prosody><break time=""0.1s"" strength=""weak""/>
+		estas son algunas de las cosas que puedes hacer con Vodafone.
+			Consultar la programación de tu televisión,<break time=""0.1s"" strength=""strong""/> buscar un móvil nuevo o<break time=""0.1s""/> comprobar que la tarifa que más se adapta a ti.
+		<break time=""0.1s"" strength=""weak""/>¿Con qué nos ponemos?
+	</speak>";
 		const { responseBuilder } = handlerInput;
 
 		return responseBuilder.speak(speechText).reprompt(speechText).getResponse();
